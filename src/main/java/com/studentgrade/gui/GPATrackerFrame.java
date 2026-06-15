fe84516fc5bcf922dfdf9e414991478faff48094
Asad Ullah <asadullahdev5@gmail.com>
2026-06-15 15:04:17 +0500

    Initial commit

---
+++ src/main/java/com/studentgrade/gui/GPATrackerFrame.java
@@ -0,0 +1,11 @@
+package com.studentgrade.gui;
+
+import javax.swing.*;
+
+public class GPATrackerFrame extends JFrame {
+    public static void main (String[] args){
+        JFrame frame = new JFrame ( "GPA Tracker" );
+        frame.setSize ( 400 ,300 );
+        frame.setVisible ( true );
+    }
+}
